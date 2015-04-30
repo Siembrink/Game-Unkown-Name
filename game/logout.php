68c0bbf3a40e91c19412d0daff00e1b81aad8618
Siembrink <s1079813@student.windesheim.nl>
2015-04-30 14:23:52 +0200

    Fixed logout.php
    title says it already.

--- game/logout.php
+++ game/logout.php
@@ -1,5 +1,5 @@
 <?php
-
+include ("../includes/config.php");
 $name = $_SESSION['name'];
 $update = "UPDATE player SET online = 0 WHERE player_name = '$name'";
 mysqli_query($connection_world, $update);
